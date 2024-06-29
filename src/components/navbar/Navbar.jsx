c99f5f61c85e0788442c476e90d92bbf53622ef9
VenkateshSircilla123 <sircillavenkatesh0@gmail.com>
2024-06-29 22:52:20 +0530

    new changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,40 @@
-import './navbar.css'
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import { AuthContext } from '../../context/AuthContext'
+import "./navbar.css";
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import axios from "axios";
 export default function Navbar() {
-  const {user} = useContext(AuthContext)
+  const { user, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const hanndleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    alert("LOGOUT successfull");
+    navigate("/");
+  };
   return (
-    <div className='navbar'>
-        <div className='navbarContainer'>
-          <Link to='/' style={{color:"inherit", textDecoration:"none"}}>
-            <span className='logo'>VS-Booking</span>
+    <div className="navbar">
+      <div className="navbarContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">VS-Booking</span>
+        </Link>
+        {!user ? (
+          <div className="navbarItems">
+            <Link to="/register">
+              <button className="navButton">Register</button>
+            </Link>
+            <Link to="/login">
+              <button className="navButton">Login</button>
+            </Link>
+          </div>
+        ) : (
+          <Link to="/">
+            <button className="navButton" onClick={hanndleLogout}>
+              Logout
+            </button>
           </Link>
-            {!user && <div className='navbarItems'>
-              <Link to='/register'>
-                <button className='navButton'>Register</button>
-              </Link>
-              <Link to='/login'>
-                <button className='navButton'>Login</button>
-              </Link>
-            </div>}
-        </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
